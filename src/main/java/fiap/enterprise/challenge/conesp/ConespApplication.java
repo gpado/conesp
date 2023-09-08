b580e790dd9d3209554a05a6c4d9aae109e4f61f
Gabriel Padoveze <53236759+gpado@users.noreply.github.com>
2023-09-08 11:31:09 -0300

    Initial commit

---
+++ src/main/java/fiap/enterprise/challenge/conesp/ConespApplication.java
@@ -0,0 +1,13 @@
+package fiap.enterprise.challenge.conesp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConespApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConespApplication.class, args);
+	}
+
+}
